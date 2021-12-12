3d107c7b68cab8ad12e6ba296e16ea1e6d7aa7d8
Houssem Hannachi <houssem.hananchi@enis.tn>
2021-12-12 20:50:01 +0100

    second

--- app/views/eshop/index.php
+++ app/views/eshop/index.php
@@ -6,13 +6,13 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Home | E-Shopper</title>
-    <link href="http://localhost/eshop/public/assets/eshop/css/bootstrap.min.css" rel="stylesheet">
-    <link href="http://localhost/eshop/public/assets/eshop/css/font-awesome.min.css" rel="stylesheet">
-    <link href="http://localhost/eshop/public/assets/eshop/css/prettyPhoto.css" rel="stylesheet">
-    <link href="http://localhost/eshop/public/assets/eshop/css/price-range.css" rel="stylesheet">
-    <link href="http://localhost/eshop/public/assets/eshop/css/animate.css" rel="stylesheet">
-	<link href="http://localhost/eshop/public/assets/eshop/css/main.css" rel="stylesheet">
-	<link href="http://localhost/eshop/public/assets/eshop/css/responsive.css" rel="stylesheet">
+    <link href="<?php ASSETS?>eshop/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php ASSETS?>eshop/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php ASSETS?>eshop/css/prettyPhoto.css" rel="stylesheet">
+    <link href="<?php ASSETS?>eshop/css/price-range.css" rel="stylesheet">
+    <link href="<?php ASSETS?>eshop/css/animate.css" rel="stylesheet">
+	<link href="<?php ASSETS?>eshop/css/main.css" rel="stylesheet">
+	<link href="<?php ASSETS?>eshop/responsive.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,14 @@
-<?php
-session_start();
-include "../app/init.php";
+    <?php
 
-$app = new App();
-?>
+    session_start();
+
+    $path = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+    $path = str_replace("index.php", "", $path);
+
+    define('ROOT', $path);
+    define('ASSETS', $path . "assets/");
+
+    include "../app/init.php";
+
+
+    $app = new App();
