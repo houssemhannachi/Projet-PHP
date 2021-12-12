51d400eca9c812dad67838f6651f08702f59c2ae
Houssem Hannachi <houssem.hananchi@enis.tn>
2021-12-13 00:48:38 +0100

    7

---
+++ eshop_db.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 13 déc. 2021 à 00:48
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `eshop_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `url_address` varchar(60) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `date` datetime NOT NULL,
+  `rank` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `url_address`, `name`, `email`, `password`, `date`, `rank`) VALUES
+(22, 'iMFSvZ2jqtB2CaArq', 'Houssem Hannachi', 'houssem.hannachi@enis.tn', 'e0c9035898dd52fc65c41454cec9c4d2611bfb37', '2021-12-13 00:39:51', 'iMFSvZ2j'),
+(26, 'kVQRjYjDEvn9aN5mKKiEzeRraUqRO', 'Aymen', 'aymen@gmail.com', '0740bf65e24caf84a7c32bd475fd9e7250e84722', '2021-12-13 00:43:25', 'kVQRjYjD');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `rank` (`rank`),
+  ADD UNIQUE KEY `date` (`date`),
+  ADD KEY `url_address` (`url_address`),
+  ADD KEY `name` (`name`),
+  ADD KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
