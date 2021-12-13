0e7ded4008d7d14ba57eaba70e14bd5e0e1e8149
Houssem Hannachi <houssem.hananchi@enis.tn>
2021-12-13 17:38:37 +0100

    11

--- eshop_db.sql
+++ eshop_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 13 déc. 2021 à 16:58
+-- Généré le : lun. 13 déc. 2021 à 17:38
 -- Version du serveur : 10.4.21-MariaDB
 -- Version de PHP : 8.0.10
 
@@ -44,7 +44,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `url_address`, `name`, `email`, `password`, `date`, `rank`) VALUES
 (22, 'iMFSvZ2jqtB2CaArq', 'Houssem Hannachi', 'houssem.hannachi@enis.tn', 'e0c9035898dd52fc65c41454cec9c4d2611bfb37', '2021-12-13 00:39:51', 'admin'),
 (31, 'BwPjMIGqns1HLszh283iorcPLMDZaze3sHgfuI', 'Rafik Rekhis', 'rafik.rekhis@enis.tn', '7b9f8daacb1809b89a6768740d50a9cbbcc9edea', '2021-12-13 16:01:13', 'customer'),
-(32, 'fiz4ziLD8une6stI34HzvxAsdFyixTwOfHcs7II3VKfzLmiNeMAy6NGtytui', 'Maher Hannachi', 'maher@hannachi.tn', '7b9f8daacb1809b89a6768740d50a9cbbcc9edea', '2021-12-13 16:02:18', 'customer');
+(32, 'fiz4ziLD8une6stI34HzvxAsdFyixTwOfHcs7II3VKfzLmiNeMAy6NGtytui', 'Maher Hannachi', 'maher@hannachi.tn', '7b9f8daacb1809b89a6768740d50a9cbbcc9edea', '2021-12-13 16:02:18', 'admin');
 
 --
 -- Index pour les tables déchargées
